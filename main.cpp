8fa255e42a0dacb7443f212aed3a485a2642b2ff
victor <victor@PC-victor>
2014-05-26 14:07:26 +0200

    main OK

--- main.cpp
+++ main.cpp
@@ -4,81 +4,49 @@
  *
  * Created on 10 de octubre de 2013, 16:27
  */
-
-#include <cstdlib>
-#include <signal.h>
+#include "rofl_config.h"
 #include <rofl/platform/unix/cunixenv.h>
+#include <rofl/platform/unix/cdaemon.h>
+#include <rofl/common/cparams.h>
+#include "rofl/common/crofbase.h"
 #include "src/proxy/ALHINP.h"
-#include <sys/wait.h>
+
+
+#define ETHSWCTLD_LOG_FILE "/var/log/ALHINP.log"
+#define ETHSWCTLD_PID_FILE "/var/run/ALHINP.pid"
 
 using namespace rofl;
-using namespace std;
 
-//Handler to stop ciosrv
-void interrupt_handler(int dummy=0) {
-	//Only stop ciosrv 
-	ciosrv::stop();
-}
-
-void TCL_finish(int dummy=0) {
-	//Only stop ciosrv 
-    wait(NULL);
-    std::cout<<"Alma recogida\n";
-}
-
-int main(int argc, char** argv){
-
-
-	//Capture control+C
-	signal(SIGINT, interrupt_handler);
-        signal(SIGCLD, TCL_finish);
+int
+main(int argc, char** argv)
+{
+	rofl::cunixenv env_parser(argc, argv);
 
 	/* update defaults */
-	rofl::csyslog::initlog(
-			rofl::csyslog::LOGTYPE_FILE,//STDERR
-			rofl::csyslog::DBG,//EMERGENCY
-			std::string("./controlador.log"),
-			"an example: ");
-
-	rofl::csyslog::set_debug_level("ciosrv", "emergency");
-	rofl::csyslog::set_debug_level("cthread", "emergency");
-
-
-	rofl::ciosrv::init();
-
-	//Orchestrator instance creation
-        orchestrator orchestrator;
-        //Start listening for AGGR_DPID
-	orchestrator.rpc_listen_for_dpts(caddress(AF_INET, "158.227.98.21", 6633));
-        //Start listening for CLIENT_DPIDs
-        orchestrator.rpc_listen_for_dpts(caddress(AF_INET, "192.168.10.1", 6633));
-        //Start connection to the controller
-        std::cout<<"Trying to connect controller: 10.10.10.3@6633\n";
-        uint8_t retry_count_ctrl=0;
-        //orchestrator.rpc_connect_to_ctl(OFP10_VERSION,1,caddress(AF_INET, CONTROLLER_IP, CONTROLLER_PORT));
-      //  try{
-               orchestrator.rpc_connect_to_ctl(OFP10_VERSION,1,caddress(AF_INET, CONTROLLER_IP, CONTROLLER_PORT));
-                
-      //  }catch(eSocketError, eSocketConnectFailed){
-      //          std::cout<<"Trying to connect controller @ 10.10.10.3@6633\n";
-      //          sleep(5);
-      //  }  
+	env_parser.add_option(coption(true,REQUIRED_ARGUMENT,'l',"logfile","Log file used when daemonization", ETHSWCTLD_LOG_FILE));
+	env_parser.add_option(coption(true, REQUIRED_ARGUMENT, 'p', "pidfile", "set pid-file", std::string(ETHSWCTLD_PID_FILE)));
         
-	
-        //ciosrv run. Only will stop in Ctrl+C
-	ciosrv::run();
+#ifdef ROFL_HAVE_OPENSSL
+	env_parser.add_option(coption(true, REQUIRED_ARGUMENT, 't', "cert-and-key-file", "Certificate and key to encrypt control traffic (PEM format)", std::string("")));
+#endif
+	//Parse
+	env_parser.parse_args();
 
-	//Printing nice trace
-	printf("\nCleaning the house...\n");
-        fflush(stdout);
-        
-	//ciosrv destroy
-	ciosrv::destroy();
-	printf("House cleaned!\nGoodbye\n");
-        fflush(stdout);
-	
-	exit(EXIT_SUCCESS);
+	if (not env_parser.is_arg_set("daemonize")) {
+		// only do this in non
+		std::string ident(env_parser.get_arg("logfile"));
+
+		logging::init();
+		rofl::logging::set_debug_level(atoi(env_parser.get_arg("debug").c_str()));
+	} else {
+
+		rofl::cdaemon::daemonize(env_parser.get_arg("pidfile"), env_parser.get_arg("logfile"));
+		rofl::logging::set_debug_level(atoi(env_parser.get_arg("debug").c_str()));
+		rofl::logging::notice << "[ethswctld][main] daemonizing successful" << std::endl;
+	}
+        ALHINP proxy;
+	cioloop::run();
+        cioloop::shutdown();
+
+	return 0;
 }
-
-
-
